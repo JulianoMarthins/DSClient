c43f3c1ba645592471c54b1187108345a108b020
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-02-24 22:48:49 -0300

    Criação da classe Client
    Foi criada a classe Client e realizada seu mapeamento para o banco de dados.
    
    O mesmo foi testado e esta funcionando perfeitamente.

---
+++ src/main/java/com/DevSuperior/DSClient/entities/Client.java
@@ -0,0 +1,104 @@
+package com.DevSuperior.DSClient.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String cpf;
+    private Double income;
+    private LocalDate brithDate;
+    private Integer children;
+
+
+    // Construtores
+    public Client(){
+
+    }
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate brithDate, Integer children){
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.brithDate = brithDate;
+        this.children = children;
+    }
+
+
+    // region Equals & HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+    //endregion
+
+
+    // region Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBrithDate() {
+        return brithDate;
+    }
+
+    public void setBrithDate(LocalDate brithDate) {
+        this.brithDate = brithDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+    // endregion
+}
