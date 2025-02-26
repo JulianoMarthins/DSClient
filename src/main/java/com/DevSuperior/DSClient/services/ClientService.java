a5044f459f635b5de222854d60a0630149acde9b
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-02-25 23:01:19 -0300

    Criação do FindAll
    Foi criado o método FindAll, os testes foram realizados no postman sendo positivo para a função e suas possiveis excessoes

--- src/main/java/com/DevSuperior/DSClient/services/ClientService.java
+++ src/main/java/com/DevSuperior/DSClient/services/ClientService.java
@@ -5,8 +5,11 @@ import com.DevSuperior.DSClient.entities.Client;
 import com.DevSuperior.DSClient.repositories.ClientRepository;
 import com.DevSuperior.DSClient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class ClientService {
@@ -23,6 +26,11 @@ public class ClientService {
     }
 
     //TODO: implementar método findAll
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(ClientDTO::new);
+    }
 
     // TODO: implementar método insert
 
